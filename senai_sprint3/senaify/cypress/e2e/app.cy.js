a40364210fd642b04308409a6e3aa547f1bd1e99
Guilherme De Campos Ribeiro <guilherme.campos.r@gmail.com>
2024-05-20 17:21:24 -0300

    Feat: Inicio da sprint de testes

---
+++ senai_sprint3/senaify/cypress/e2e/app.cy.js
@@ -0,0 +1,52 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+
+  })
+
+  it('Verificar está exibido', () => {
+    cy.get("[aria-label= 'title-head']")
+    cy.get("[aria-label= 'title-head']").should("contain", "Good morning")
+
+  })
+
+  it('Verificar se tem uma lista com as playlist exibida', () => {
+    cy.wait(2000)
+    cy.get("[aria-label= 'playlist-item']").should("have.length.greaterThan", 0)
+
+  })
+  it('Clicar em uma opção e listar suas musicas', () => {
+    cy.get("[aria-label= 'playlist-item']").contains("Pagodeira").click();
+    cy.wait(1000)
+    cy.get("[aria-label= 'music-item']").should("have.length.greaterThan", 0)
+
+  })
+  it('Clicar em uma opção e tocar musica', () => {
+    cy.get("[aria-label= 'music-item']").contains("Vamo de Pagodin").click()
+    cy.scrollTo("top")
+  })
+
+  // it('Verificar está exibido', () => {
+  //   cy.get("[aria-label= 'title-head']")
+  //   cy.get("[aria-label= 'title-head']").should("contain", "Good morning")
+
+  // })
+
+  // it('Verificar se tem uma lista com as playlist exibida', () => {
+  //   cy.wait(2000)
+  //   cy.get("[aria-label= 'playlist-item']").should("have.length.greaterThan", 0)
+
+  // })
+  // it('Clicar em uma opção e listar suas musicas', () => {
+  //   cy.get("[aria-label= 'playlist-item']").first().click()
+  //   cy.wait(1000)
+  //   cy.get("[aria-label= 'music-item']").should("have.length.greaterThan", 0)
+
+  // })
+  // it('Clicar em uma opção e tocar musica', () => {
+  //   cy.get("[aria-label= 'music-item']").eq(4).click()
+  //   cy.scrollTo("top")
+
+  // })
+
+})
